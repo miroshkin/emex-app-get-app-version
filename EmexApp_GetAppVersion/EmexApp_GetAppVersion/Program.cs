66d10cb89a6e7c7f5016d8a46984654ae405f0c3
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-01-31 17:18:49 +0300

    Deleted link activation

--- EmexApp_GetAppVersion/EmexApp_GetAppVersion/Program.cs
+++ EmexApp_GetAppVersion/EmexApp_GetAppVersion/Program.cs
@@ -9,10 +9,9 @@ namespace EmexApp_GetAppVersion
 {
     class Program
     {
-        [STAThread]
+        
         static void Main(string[] args)
         {
-            System.Diagnostics.Process.Start("http://tfs.emexmsk.local:8080/tfs/emex/v1.0/_apps/hub/ms.vss-ciworkflow.build-ci-hub?_a=edit-build-definition&id=611");
             var date = DateTime.Now;
             Clipboard.SetText(date.ToString("yyyy.MMdd.HHmm"));
         }
