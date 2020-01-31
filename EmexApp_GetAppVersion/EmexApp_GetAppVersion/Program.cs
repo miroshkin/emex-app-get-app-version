3e509f47890e5a009fc0144dff5344fd8cb1ca85
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-01-31 16:50:05 +0300

    Added GetVersion micro app

---
+++ EmexApp_GetAppVersion/EmexApp_GetAppVersion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmexApp_GetAppVersion
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("http://tfs.emexmsk.local:8080/tfs/emex/v1.0/_apps/hub/ms.vss-ciworkflow.build-ci-hub?_a=edit-build-definition&id=611");
+            var date = DateTime.Now;
+            Clipboard.SetText(date.ToString("yyyy.MMdd.HHmm"));
+        }
+    }
+}
